a952c103b758bbc93957b2b43208c714cef80fe3
Walid KAHLOUN <wkahloun1@gmail.com>
2025-01-29 15:41:18 +0100

    Finding Element Using ID _ LoginExemple

---
+++ src/test/java/findingElementUsingID.java
@@ -0,0 +1,45 @@
+// Import WebDriverManager for automatic WebDriver setup
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+// Import WebDriver interface from Selenium
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+// Import ChromeDriver class for working with Google Chrome browser
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Define a public class named 'findingElementUsingID'
+public class findingElementUsingID {
+
+    // Main method where execution starts
+    public static void main(String[] args) throws InterruptedException {
+
+        // Setup ChromeDriver using WebDriverManager
+        // (This automatically downloads and configures the correct ChromeDriver version)
+        WebDriverManager.chromedriver().setup();
+
+        // Create an instance of ChromeDriver to launch the Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize the browser window to fit the screen
+        driver.manage().window().maximize();
+
+        // Open the login page of the test website
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        // Locate the username input field using its ID and enter the username "tomsmith"
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+
+        // Locate the password input field using its ID and enter the password "SuperSecretPassword!"
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+        // Locate the login button using its class name and click on it
+        driver.findElement(By.className("radius")).click();
+
+        // Wait for 2 seconds (2000 milliseconds) to observe the result before proceeding
+        Thread.sleep(2000);
+
+        // Close the browser and terminate the WebDriver session
+        driver.quit();
+    }
+}
